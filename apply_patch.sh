d73c91ff3ea9941a79f949f9e5a1fda6ae2c9aa3
Charlie Naseby <c.naseby18@imperial.ac.uk>
2025-02-10 14:08:37 +0000

    Added a patch for BDSIM to allow for fitting and updated install instructions

---
+++ apply_patch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+patch -p1 -d /home/bdsim/jairhul-bdsim-c725b23739b8/ < t2k_bdsim_modifications.diff
+
+cd /home/bdsim/bdsim-build/
+cmake ../jairhul-bdsim-c725b23739b8/
+make
+make install
